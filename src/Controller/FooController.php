16cddf8f1cf094ef476a1f5710012c7987c0d0d6
hrebelnyi <hrebelnyi@gmail.com>
2018-06-03 14:03:16 +0300

    Playing with controllers

--- src/Controller/FooController.php
+++ src/Controller/FooController.php
@@ -4,16 +4,47 @@ namespace App\Controller;
 
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 class FooController extends AbstractController
 {
     /**
-     * @Route("/welcome", name="foo")
+     * @Route("/welcome/{name}", 
+     *		name="foo", 
+     *		defaults = {"name"="Whatever1"}, 
+     *		requirements={"name"="[A-Za-z]+"})
+     * @param Request
+     * @return Response
      */
-    public function index()
+    //public function index(Request $request)
+    public function index(string $name)
     {
-        return $this->render('foo/index.html.twig', [
-            'controller_name' => 'FooController',
-        ]);
+
+    	//app.request.query.get('name') | default('World')	
+    	//$some_var = $request->query->get("name");	
+
+        return $this->render('foo/index.html.twig',
+        	[ 
+        		//'name' => $some_var
+        		'name' => $name
+        	]
+    	);
+    }
+
+
+    /**
+     * @Route("/", name="hello_page")
+     */
+    public function hello_page()
+    {
+        return $this->render('hello_page.html.twig');
+    }
+
+    /**
+     * @Route("/test", name="test")
+     */
+    public function test()
+    {
+        return $this->render('foo/test.html.twig');
     }
 }
